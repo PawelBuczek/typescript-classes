78a35b63ae69b97e1e6f8e105aaa4ac286e0cb92
PawelBuczek <pawel.buczek@email.com>
2024-01-22 10:26:59 +0100

    Shape base class added

---
+++ shapes_inheritance/Shape.ts
@@ -0,0 +1,21 @@
+
+export class Shape {
+    constructor(private _x: number, private _y:number) {
+    }
+
+    private get x(): number {
+        return this._x;
+    }
+
+    private get y(): number {
+        return this._y;
+    }
+
+    private set x(value: number) {
+        this._x = value;
+    }
+
+    private set y(value: number) {
+        this._y = value;
+    }
+}
