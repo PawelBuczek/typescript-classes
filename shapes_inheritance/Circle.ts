75ff832c035f1f6463bda2fd441294c8f12635fe
PawelBuczek <pawel.buczek@email.com>
2024-01-22 18:01:26 +0100

    extended Shape with Rectangle

--- shapes_inheritance/Circle.ts
+++ shapes_inheritance/Circle.ts
@@ -2,7 +2,7 @@ import { Shape } from "./Shape";
 
 export class Circle extends Shape {
 
-    constructor(private theX: number, private theY: number, private _radius: number) {
+    constructor(theX: number, theY: number, private _radius: number) {
         super(theX, theY);
         this._radius = _radius;
     }
---
+++ shapes_inheritance/Rectangle.ts
@@ -0,0 +1,14 @@
+import { Shape } from "./Shape";
+
+export class Rectangle extends Shape {
+
+    constructor(theX: number, theY: number, private _width: number, private _length: number) {
+        super(theX, theY);
+        this._width = _width;
+        this._length = length;
+    }
+
+    getInfo(): string {
+        return super.getInfo + `, width=${this._width}, length=${this._length}`;
+    }
+}
