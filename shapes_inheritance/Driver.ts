67ec050666f0b5be0efda365577fbac2b1aa8e81
PawelBuczek <pawel.buczek@email.com>
2024-01-22 18:38:05 +0100

    modified Driver.ts

--- shapes_inheritance/Driver.ts
+++ shapes_inheritance/Driver.ts
@@ -2,15 +2,21 @@ import { Circle } from "./Circle";
 import { Rectangle } from "./Rectangle";
 import { Shape } from "./Shape";
 
-let shape: Shape = new Shape(2, 3);
 let circle: Circle = new Circle(4, 5, 6);
 let rectangle: Rectangle = new Rectangle(7, 8, 9, 10);
 
 let shapeCollection: Shape[] = [];
-shapeCollection.push(shape);
 shapeCollection.push(circle);
 shapeCollection.push(rectangle);
 
 console.log(shapeCollection);
 
+// using collection with objects extending abstract class Shape
+
 shapeCollection.forEach(sh => console.log(sh.getInfo()));
+
+console.log();
+
+for (let sh of shapeCollection) {
+    console.log(sh.calculateArea());
+}
